4ebc931848969c7f28fb12f9edb47ef9a70c8c11
avrilzchen <a366chen@uwaterloo.ca>
2023-05-12 18:41:30 -0400

    wra

--- server-chatgpt-youtube/index.js
+++ server-chatgpt-youtube/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 // Configure open api
 const configuration = new Configuration({
-    organization: "PASTE YOUR ORGANIZATION ID HERE",
+    organization: "org-2aznMr7JYxM1NNuyJlueA41x",
     apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
 })
 const openai = new OpenAIApi(configuration)
